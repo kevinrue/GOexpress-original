b7d7eb8e3a8df10b7b0b23eca666027b2b0c0a9d
Kevin Rue <kevin.rue@ucdconnect.ie>
2014-04-14 03:29:44 +0100

    Major addition of features. Plots added (cluster, heatmap). Package structure greatly enhanced. All checks, except PDF manual are OK.
    Minor work left for later maintenance.

--- R/GO_anova_generic.R
+++ R/GO_anova_generic.R
@@ -30,15 +30,14 @@
 ## library(stringr) # pattern extraction
 
 
-GO_anova = function(expr_data, phenodata, f, biomart_dataset="", adj.P.method = "BH"){
-  library(biomaRt, quietly=TRUE)
+GO_anova = function(expr_data, phenodata, f, biomart_dataset="", adj.P.method = "BH", FDR=0.05){
   # Connect to the appropriate biomaRt for the expression data or the user specified dataset
-  cat("Connecting to appropriate biomaRt dataset...", fill=TRUE)
-  mart = getMart(expr_data, biomart_dataset)
+  cat("Connecting to appropriate BioMart dataset ...", fill=TRUE)
+  mart = get_mart_dataset(expr_data, biomart_dataset)
   # Information for the user
   print(mart)
   # Prepare a table mapping the ensembl ids in the expression dataset to GO terms (biological processes)
-  cat("Fetching ensembl gene/GO term mapping from biomaRt...", fill=TRUE)
+  cat("Fetching ensembl_gene/GO_term mapping from BioMart ...", fill=TRUE)
   GO_genes = getBM(attributes=c("ensembl_gene_id", "go_id"),
                    filters="ensembl_gene_id",
                    values=rownames(expr_data),
@@ -47,33 +46,33 @@ GO_anova = function(expr_data, phenodata, f, biomart_dataset="", adj.P.method = 
   # Remove over 1,000 rows where the go_id is ""
   GO_genes = subset(GO_genes, go_id != "")
   # Calculate the F.value and p.value of ANOVA for each ensembl id in the expression dataset
-  cat("Calculating ANOVA for", nrow(expr_data), "genes. This may take a few minutes... (about 2min for 12,000 genes)", fill=TRUE)
-  res_anova = data.frame("F.value"= apply(X=log2_cpm_norm_no0H,
+  cat("Calculating ANOVA for", nrow(expr_data), "genes. This may take a few minutes ... (about 2min for 12,000 genes)", fill=TRUE)
+  res_anova = data.frame("F.value"= apply(X=expr_data,
                                           MARGIN=1,
                                           FUN=function(x){oneway.test(formula=expr~group,
-                                                                      data=cbind(expr=x, group=pData(targets_no0H)[,f]))$statistic}),
-                         "p.value"=apply(X=log2_cpm_norm_no0H,
+                                                                      data=cbind(expr=x, group=pData(phenodata)[,f]))$statistic}),
+                         "p.value"=apply(X=expr_data,
                                          MARGIN=1,
                                          FUN=function(x){oneway.test(formula=expr~group,
-                                                                     data=cbind(expr=x, group=pData(targets_no0H)[,f]))$p.value}))
+                                                                     data=cbind(expr=x, group=pData(phenodata)[,f]))$p.value}))
   # Correct for multiple testing (default: BH)
   res_anova$FDR = p.adjust(p=res_anova$p.value, method=adj.P.method)
   # Set to 0 the F.value of non-significant genes
-  res_anova$F.value[res_anova$FDR > 0.05] = 0
+  res_anova$F.value[res_anova$FDR > FDR] = 0
   # Summary statistics by GO term
   ## Merge the table mapping GOterm to genes with the ANOVA results of each gene
   GO_gene_anova = merge(x=GO_genes, y=res_anova, by.x="ensembl_gene_id", by.y="row.names")
   # Results can now be summarised by aggregating rows with same GOterm
   ## Prepare a table of all the GO terms linked to at least a gene in the expression data
-  cat("Fetching GO terms description from biomaRt...", fill=TRUE)
+  cat("Fetching GO_terms description from BioMart ...", fill=TRUE)
   all_GO = getBM(attributes=c("go_id", "name_1006", "namespace_1003"),
                  filters="ensembl_gene_id",
-                 values=rownames(log2_cpm_norm_no0H),
+                 values=rownames(expr_data),
                  mart=mart,
                  uniqueRows=TRUE)
   # Remove one of the GO terms which is ""
-  all_GO = subset(all_GO, go_id != "")
-  cat("Merging score into result table...", fill=TRUE)
+  all_GO = all_GO[all_GO$go_id != "",]
+  cat("Merging score into result table ...", fill=TRUE)
   ## Average F value (+) robust for GO terms with several genes (5 minimum advised, 10 was found robust, gene counts per GO term below)
   GO_scores = merge(x=aggregate(F.value~go_id, data=GO_gene_anova, FUN=mean), y=all_GO, by="go_id")
   colnames(GO_scores)[2] = "ave.F.score"
@@ -90,57 +89,24 @@ GO_anova = function(expr_data, phenodata, f, biomart_dataset="", adj.P.method = 
   colnames(GO_scores)[2] = "gene_count"
   # Rank the GO terms by decreasing average F value
   GO_scores = GO_scores[order(GO_scores$ave.F.score, decreasing=TRUE),]
+  # Appends gene annotations to rows of res_anova
+  genes_anova = merge(x=res_anova,
+        y=getBM(attributes=c("ensembl_gene_id", "external_gene_id", "description"),
+                             filters="ensembl_gene_id",
+                             values=rownames(res_anova),
+                             mart=mart),
+        by.x="row.names",
+        by.y="ensembl_gene_id")
+  # Rank the genes by increasing FDR (should match increasing F.value)
+  genes_anova = genes_anova[order(genes_anova$FDR),]
+  # Put the ensembl identifier back as the row name
+  rownames(genes_anova) = genes_anova$Row.names
+  genes_anova$Row.names = NULL
   # Return the results of the analysis
-  return(list(scores=GO_scores, mapping=GO_genes))
+  return(list(scores=GO_scores, mapping=GO_genes, anova=genes_anova, factor=f))
 }
 
-hist.scores = function(result){
-  hist(result$scores$ave.F.score)
-}
-
-quantiles.scores = function(result, quartiles=c(FALSE, TRUE)){
-  # If user changes to "quartiles=TRUE" then the following will be true
-  if (quartiles[1]){
-    quantile(x=result$scores$ave.F.score)
-  }
-  # Default is to give range of top 10%, 5%, 1%, 0.1% and 0.01%
-  else{
-    quantile(x=result$scores$ave.F.score, c(0.9, 0.95, 0.99, 0.999, 0.9999))
-  }
-}
-
-show.scores = function(result){
-  result$scores
-}
-
-subset.scores = function(result, ...){
-  print(list(...))
-  print(names(list(...)))
-  #print(head(result$scores))
-  # Save the list of filter and value for easier referencing
-  filters = list(...)
-  # prepares a table where the filters will be saved
-  filtered = data.frame(row.names=result$scores$go_id)
-  # For each filter
-  for (filter in names(list(...))){
-    # Check that it is a valid filter
-    if (!filter %in% colnames(result$scores)){
-      stop(filter, " is not a valid column name.")
-    }
-    # Save the filter status of each row for this filter
-    ## Filters where the value should be higher than the given threshold
-    if (filter %in% c("gene_count", "gene_count", "ave.F.score")){
-      cat(filter, " equal or more than:", filters[[filter]], fill=TRUE)
-      filtered[,filter] = result$scores[,filter] >= filters[filter]
-    }
-  }
-  #
-  filtered$merge = apply(X=filtered, MARGIN=1, FUN=all)
-  #
-  list(scores=subset(result$score, filtered$merge), mapping=result$mapping)
-}
-
-getMart = function(expr_data, biomart_dataset){
+get_mart_dataset = function(expr_data, biomart_dataset){
   # This function returns a mart from biomaRt
   ## either the dataset specified by the user if any
   ## or the dataset corresponding to the species guessed from an gene id in the expression data
@@ -155,7 +121,6 @@ getMart = function(expr_data, biomart_dataset){
     sample_gene = rownames(expr_data)[1]
     # If the gene id starts by "ENS" (most cases)
     if (length(grep(pattern="^ENS", x=sample_gene))){
-      library(stringr)
       # Extract the full prefix
       prefix = str_extract(sample_gene, "ENS[[:upper:]]+")
       # load the precomputed table mapping prefix to biomart database
@@ -183,7 +148,7 @@ getMart = function(expr_data, biomart_dataset){
     # If the gene id starts with "Y"
     else if (length(grep(pattern="^Y", x=sample_gene))) {
       # load the corresponding biomart dataset
-      retur(useMart(biomart="ensembl", dataset="scerevisiae_gene_ensembl"))
+      return(useMart(biomart="ensembl", dataset="scerevisiae_gene_ensembl"))
     }
     # If the gene id does not match any known ensembl gene id prefix, return an error and stop
     else{
---
+++ R/post_analysis.R
@@ -0,0 +1,87 @@
+hist_scores = function(result, ...){
+  hist(result$scores$ave.F.score, ...)
+}
+
+quantiles_scores = function(result, quartiles=c(FALSE, TRUE)){
+  # If user changes to "quartiles=TRUE" then the following will be true
+  if (quartiles[1]){
+    quantile(x=result$scores$ave.F.score)
+  }
+  # Default is to give range of top 10%, 5%, 1%, 0.1% and 0.01%
+  else{
+    quantile(x=result$scores$ave.F.score, c(0.9, 0.95, 0.99, 0.999, 0.9999))
+  }
+}
+
+subset_scores = function(result, ...){
+  # Save the list of filter and value for easier referencing
+  filters = list(...)
+  # prepares a table where the filters will be saved
+  filtered = data.frame(row.names=result$scores$go_id)
+  # For each filter
+  for (filter in names(list(...))){
+    # Check that it is a valid filter
+    if (!filter %in% colnames(result$scores)){
+      stop(filter, " is not a valid column name.")
+    }
+    # Save the filter status of each row for this filter
+    ## Filters where the value should be higher than the given threshold
+    if (filter %in% c("gene_count", "gene_count", "ave.F.score")){
+      #cat(filter, "equal or more than", filters[[filter]], fill=TRUE)
+      filtered[,filter] = result$scores[,filter] >= filters[filter]
+    }
+  }
+  #
+  filtered$merge = apply(X=filtered, MARGIN=1, FUN=all)
+  #
+  list(scores=subset(result$score, filtered$merge), mapping=result$mapping)
+}
+
+list_genes = function(result, go_id){
+  # If the GO term requested is not present in the dataset
+  if (!go_id %in% result$mapping$go_id){
+    # return an error and stop
+    stop(go_id, "is not present in the dataset.")
+  }
+  # Otherwise return the list of ensembl_ids associated with it
+  return(result$mapping[result$mapping$go_id == go_id,]$ensembl_gene_id)
+}
+
+heatmap_GO = function(go_id, result, expr_data, phenodata, gene_names=TRUE,
+                      f=result$factor, scale="none", cexCol=1.2, cexRow=1, 
+                      main=paste(go_id, result$scores[result$scores$go_id == go_id,]$name_1006),
+                      ...){
+  # Fetch the list of genes associated with the go_id
+  ensembl_ids = list_genes(result, go_id)
+  # Fetch and transform the expression data for those genes
+  genes_expr = t(expr_data[ensembl_ids,])
+  # Rows are samples, label them according to the user's choice
+  sample_labels = pData(phenodata)[,f]
+  # Columns are genes, label them by identifier or name
+  if (gene_names){
+    gene_labels = result$anova[ensembl_ids,]$external_gene_id
+  }
+  else{
+    gene_labels = ensembl_ids
+  }
+  # Plot the heatmap of the data
+  heatmap(genes_expr, labRow=sample_labels, labCol=gene_labels, scale=scale,
+          cexCol=cexCol, cexRow=cexRow, main=main, ...)
+}
+
+cluster_GO = function(go_id, result, expr_data, phenodata, f=result$factor, 
+                      method_dist="euclidean", method_hclust="average", cex=0.5,
+                      main=paste(go_id, result$scores[result$scores$go_id == go_id,]$name_1006),
+                      ...){
+  # Fetch the list of genes associated with the go_id
+  ensembl_ids = list_genes(result, go_id)
+  # Fetch and transform the expression data for those genes
+  genes_expr = t(expr_data[ensembl_ids,])
+  # Hierarchical clustering
+  # (The clearest to read the labels and control their size)
+  di <- dist(genes_expr, method=method_dist, ...) # euclidean distances between the rows
+  cl <- hclust(di, method=method_hclust, ...)
+  # Rows are samples, label them according to the user's choice
+  sample_labels = pData(phenodata)[,f]
+  plot(cl, hang=-1, label=sample_labels, cex=cex, main=main, ...)
+}
